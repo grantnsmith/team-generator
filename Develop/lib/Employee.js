803d54e5b202dce65bf7eb470229e782a4677e83
Grant Smith <grantnsmith@gmail.com>
2020-05-03 11:43:28 -0700

    Created all classes. Passed all class tests.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,4 +1,3 @@
-// TODO: Write code to define and export the Employee class
 class Employee {
     constructor(name, id, email) {
         this.name = name;
@@ -8,14 +7,14 @@ class Employee {
     getName() {
         return this.name;
     }
-    getID() {
+    getId() {
         return this.id;
     }
     getEmail() {
         return this.email;
     }
     getRole() {
-        return Employee;
+        return "Employee";
     }
 }
 
